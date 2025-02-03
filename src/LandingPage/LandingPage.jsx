e3e72cd34696cd4183996efccbf64bb473e78a61
aRonnieAlsop <alsop.arron@gmail.com>
2025-02-02 21:47:15 -0800

    'wip'

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -4,7 +4,9 @@ import './LandingPage.css';
 const LandingPage = () => {
     return (
         <div>
-            
+            {/* image 👇🏼 for full-span of landing page: */}
+            <img src='./assets/wood_floor_PAWF_2.jpg' className="fullscreen-image" />
+            <div className="image-overlay"></div>
         </div>
     );
 }
