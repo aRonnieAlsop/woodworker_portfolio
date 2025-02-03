fd5a9e5390a7ed850581527534f971bb7d6e080e
aRonnieAlsop <alsop.arron@gmail.com>
2025-02-02 22:16:55 -0800

    'wip'

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -7,6 +7,14 @@ const LandingPage = () => {
             {/* image 👇🏼 for full-span of landing page: */}
             <img src='./assets/wood_floor_PAWF_2.jpg' className="fullscreen-image" />
             <div className="image-overlay"></div>
+            {/* Logo 👇🏼*/}
+            <img src='./assets/logo_PAWF.png' alt="Logo" className="logo" />
+
+            <div className="locations-container">
+                <div className="image-text">Sonoma</div>
+                <div className="image-text">Napa</div>
+                <div className="image-text">Marin</div>
+            </div>
         </div>
     );
 }
