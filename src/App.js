a295df941fc0c0f1c1843f31df3b31e36b776b12
Ronnie Alsop <alsop.arron@gmail.com>
2025-03-27 15:13:40 -0700

    'maps wip'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Header from "./components/Header/Header";
@@ -6,11 +6,13 @@ import MockContact from "./components/Contact/MockContact";
 import About from "./components/About/About";
 
 const App = () => {
+    const mapSectionRef = useRef(null);
+
     return (
         <Router>
-            <Header />
+            <Header apSectionRef={mapSectionRef} />
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<LandingPage mapSectionRef={mapSectionRef} />} />
                 <Route path="/contact" element={<MockContact />} />
                 <Route path="/about" element={<About />} />
             </Routes>
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import Header from './../Header/Header';
 import Hero from './../Hero/Hero';
 import RelocationPopup from '../../RelocationPopUP/RelocationPopup';
 import Gallery from '../Gallery/Gallery';
